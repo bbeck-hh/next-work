799def285bc4330dbf654fd7c8902ffc037a87c2
Bjoern <beck.b.hh@gmail.com>
2025-05-21 12:23:03 +0200

    Enhance global styles with Tailwind CSS; integrate Montserrat font and update theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#f3e7ce',
+        secondary: '#240707'
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [daisyui],
 };
